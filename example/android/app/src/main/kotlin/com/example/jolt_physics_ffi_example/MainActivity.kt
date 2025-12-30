fb59722a129076b6a3b3d04661356ebdc3940f1f
tguerin <contact@7omtech.fr>
2025-12-30 19:11:22 +0100

    Initial plugin setup with FFI template

---
+++ example/android/app/src/main/kotlin/com/example/jolt_physics_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jolt_physics_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Classes/jolt_physics_ffi.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../jolt_physics_ffi.podspec for more information.
+#include "../../src/jolt_physics_ffi.c"
